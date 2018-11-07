3b6a3fb5995b95c1d9e4450c14274a8c5049ba4e
jeremythia <jeremythia@icloud.com>
2018-11-08 01:35:17 +0800

    added getInfo for visualization
    init

--- QEPVisualizer/drawing_tools.py
+++ QEPVisualizer/drawing_tools.py
@@ -1,17 +1,17 @@
 import json
+import os
 from tkinter import *
 from Node import Node
 
 
-NODE_WIDTH=50
-NODE_HEIGHT=50
+NODE_WIDTH=100
+NODE_HEIGHT=100
 CANVAS_WIDTH=1000
 CANVAS_HEIGHT=1000
 node_list=[]
 instance_dict={}
 # Maps GUI elements to logical elements
 visual_to_node={}
-
 
 def clicked(event,canvas):
     '''
@@ -25,13 +25,8 @@ def clicked(event,canvas):
         canvas.delete(instance_dict[node])
         del instance_dict[node]
     else:
-
-        instance=canvas.create_text((node.center[0],node.center[1]-(NODE_HEIGHT/2)-10),text=node.text,fill="blue")
+        instance=canvas.create_text((node.center[0] + 130,node.center[1]-(NODE_HEIGHT/2)+50),text=node.text,fill="blue")
         instance_dict[node]=instance
-
-
-
-
 
 def draw_query_plan(data):
     '''
@@ -39,16 +34,15 @@ def draw_query_plan(data):
     :param data: json object
 
     '''
-    #data = open(data).read()
-    #data= json.loads(data)[0]['Plan']
-
-    node_list.append(Node(0, CANVAS_WIDTH,50, NODE_HEIGHT))
+    # data = open(path).read()
+    # data= json.loads(data)[0]['Plan']
+    node_list.append(Node(0, CANVAS_WIDTH,150, NODE_HEIGHT))
     build_node_list(data, node_list[0])
     # actual drawing
     root = Tk()
     root.geometry("1000x1000")
     root.title("Query execution plan")
-    canvas = Canvas(root, width=1000, height=1000)
+    canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
     canvas.pack()
     # 3 different for loops are needed for logical binding of rectangles in the node_list
     for element in node_list:
@@ -70,9 +64,6 @@ def draw_query_plan(data):
 
     root.mainloop()
 
-
-
-
 def build_node_list(plan, obj):
     '''
     BUilds the list of nodes by recursively calling itself
@@ -88,3 +79,179 @@ def build_node_list(plan, obj):
             node_list.append(child_obj)
             obj.children.append(child_obj)
             build_node_list(plan["Plans"][i], child_obj)
+
+def getInfo(plan):
+    ## takes a particular plan and checks for node type to get info
+    ## code to get json object (node type etc) to be inserted
+    parsed_plan = "" ## sentence/info to return
+    
+    if (plan["Node Type"] == "Aggregate"):
+        if plan["Strategy"] == "Sorted": 
+            if "Group Key" in plan:
+                parsed_plan += "The attributes used for grouping the result are"
+                for group_key in plan["Group Key"]:
+                    parsed_plan += group_key.replace("::text", "") + ", "
+                parsed_plan = parsed_plan[:-2]
+            if "Filter" in plan:
+                parsed_plan += " and it is filtered using the condition " + plan["Filter"].replace("::text", "") + "."
+        
+        elif plan["Strategy"] == "Hashed":
+            group_keys = ','.join([j.replace("::text", "")
+                                for j in plan["Group Key"]])
+            parsed_plan += "All the rows are hashed based on the key {},".format(group_keys)
+            parsed_plan += "."
+
+        elif plan["Strategy"] == "Plain":
+            parsed_plan = 'Aggregate'
+
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Append":
+        
+        parsed_plan += "The results of the scan are appended."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "CTE Scan":
+
+        parsed_plan = "The table is scanned using CTE scan "
+        parsed_plan += str(plan["CTE Name"]) + " which is stored in memory "
+
+        if "Index Cond" in plan:
+            parsed_plan += " with condition(s) " + \
+                plan["Index Cond"].replace('::text', '') + "."
+
+        elif "Filter" in plan:
+            parsed_plan += " the filtering condition applied is " + \
+                plan["Filter"].replace('::text', '') + "."
+
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Unique":
+        parsed_plan += "Each row is scanned from the sorted data, and duplicate elements (from the preceeding row) are eliminated."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Function Scan":
+        parsed_plan += "The function {} is executed and the resulting set of tuples is returned.".format(plan["Function Name"])
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Group":
+        group_keys = ",".join([j.replace("::text", "")
+                       for j in plan["Group Key"][:-1]])
+        parsed_plan += "The result from the previous operation is grouped together using the key "
+        parsed_plan += group_keys + "."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Hash":
+        parsed_plan = "The hash function creates an in-memory hash with the tuples from the source."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Hash Join":
+        parsed_plan += "The result produced by the previous operation is joined using Hash {} Join".format(
+            plan["Join Type"])
+        if 'Hash Cond' in plan:
+            parsed_plan += ' with condition {}.'.format(plan['Hash Cond'].replace("::text", ""))
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Index Scan":
+        parsed_plan = "An index scan is performed using " + plan["Index Name"]
+        if "Index Cond" in plan:
+            parsed_plan += " with conditions {}".format(plan["Index Cond"].replace('::text', ''))
+        if "Filter" in plan:
+            parsed_plan += " The result is then filtered by {}.".format(plan["Filter"].replace('::text', ''))  
+        
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+        
+    elif plan["Node Type"] == "Index Only Scan":
+        parsed_plan = "An index scan is performed using the index on {}".format(plan["Index Name"])
+        if "Index Cond" in plan:
+            parsed_plan += " with the condition(s) {}.".format(plan["Index Cond"].replace('::text', ''))
+        if "Filter" in plan:
+            parsed_plan += ". The result is then filtered by {}.".format(plan["Filter"].replace('::text', ''))
+        
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+    elif plan["Node Type"] == "Limit":
+        planrows = plan["Plan Rows"]
+        parsed_plan += "The table scanning is done, but with a limitation of " + str(planrows) + " items."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+    
+    elif plan["Node Type"] == "Materialize":
+        parsed_plan = "Holding the results in memory enables efficient accessing."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+    
+    elif plan["Node Type"] == "Merge Join":
+        parsed_plan = "Merge join is employed to merge the preceeding result"
+        if 'Merge Cond' in plan:
+            parsed_plan += " with the following condition: " + plan['Merge Cond'].replace("::text", "") + "."
+        if 'Join Type' == 'Semi':
+            parsed_plan += " however, the only row returned is that from the left table."
+        else:
+            parsed_plan += "."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+    
+    elif plan["Node Type"] == "Nested Loop":
+        parsed_plan = "Nested Loop Join."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+    
+    elif plan["Node Type"] == "Seq Scan":
+        parsed_plan = "A sequential scan is performed on the relation "
+        if "Relation Name" in plan:
+            parsed_plan += plan['Relation Name']
+        
+        if "Alias" in plan:
+            if plan['Relation Name'] != plan['Alias']:
+                parsed_plan += " under the alias name "
+                parsed_plan += plan['Alias']
+        parsed_plan += "."
+        
+        if "Filter" in plan:
+            parsed_plan += " This is bounded by the condition, "
+            parsed_plan += plan['Filter'].replace("::text", "")[1:-1]
+            parsed_plan += "."
+
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+    
+    elif plan["Node Type"] == "Sort":
+        parsed_plan += "the result is sorted on the attribute "
+        if "DESC" in plan["Sort Key"]:
+            parsed_plan += str(plan["Sort Key"].replace('DESC','')) + " in desceding order."
+        elif "INC" in plan["Sort Key"]:
+            parsed_plan += str(plan["Sort Key"].replace('INC','')) + " in increasing order."
+        else:
+            parsed_plan += str(plan["Sort Key"]) + "."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+        
+    elif plan["Node Type"] == "Subquery Scan":
+        parsed_plan = "A subquery scan is performed on the result from the previous operation."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+    
+    elif plan["Node Type"] == "Values Scan":
+        parsed_plan += "A scan through the given values from the query is performed."
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+    
+    else:
+        parsed_plan = plan["Node Type"]
+        parsed_plan += "Duration taken: " + plan["Total Cost"] + "."
+        return parsed_plan
+
+
+
+        
