f79481043f8c1b49a50bf953594c6f2a5d693ea4
programmer1997 <ajinkya001@e.ntu.edu.sg>
2018-11-12 16:12:16 +0800

    duration

--- QEPVisualizer/Node.py
+++ QEPVisualizer/Node.py
@@ -1,5 +1,7 @@
+from random import randint
+
 class Node:
-    def __init__(self,x1,x2,y1,y2,text=""):
+    def __init__(self,x1,x2,y1,y2,text="", plan_info="",duration=0):
         self.x1=x1
         self.x2=x2
         self.y1=y1
@@ -7,6 +9,9 @@ class Node:
         self.text=text
         self.center=((x1+x2)/2,(y1+y2)/2)
         self.children=[]
+        self.plan_info=plan_info
+        self.duration=duration
+
 
     def add_child(self,child):
         self.children.append(child)
